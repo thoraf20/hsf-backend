d792275b8a71a0440a78a7b7ca7c7a3844a35e4f
samson onifade <gbemilekeadeboyega@gmail.com>
2025-03-30 08:51:42 +0100

    done with some fixes

--- src/application/useCases/Auth/Auth.ts
+++ src/application/useCases/Auth/Auth.ts
@@ -244,7 +244,6 @@ export class AuthService {
       await this.userRepository.update(user.id, { failed_login_attempts: 0 })
     }
     if (user.is_email_verified === false) {
-      await this.userRepository.update(user.id, {is_email_verified: true})
       await this.resendOtp(user.email)
       throw new ApplicationCustomError(
         StatusCodes.BAD_REQUEST,
