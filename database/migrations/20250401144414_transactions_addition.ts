9bc207d1ca2ef594865e4ac17ebfaf9ac8ae99fb
samson onifade <gbemilekeadeboyega@gmail.com>
2025-04-03 13:38:43 +0100

    done working on changes on forgotten email

--- database/migrations/20250401144414_transactions_addition.ts
+++ database/migrations/20250401144414_transactions_addition.ts
@@ -4,7 +4,6 @@ import type { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
     await knex.schema.alterTable("transactions", (table) => {
         table.uuid('property_id')
-        .notNullable()
         .references('id')
         .inTable('properties')
         .onDelete('CASCADE')
