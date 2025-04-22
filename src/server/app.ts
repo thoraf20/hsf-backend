04d6787f5b18d7fa5a518eef3bd1f19defa39947
samson onifade <gbemilekeadeboyega@gmail.com>
2025-04-22 15:52:37 +0100

    done wth the compilig delay

--- src/server/app.ts
+++ src/server/app.ts
@@ -78,5 +78,6 @@ app.use(ErrorHandler)
 
 const PORT = process.env.PORT || 3000
 server.listen(PORT, () => {
-  console.log('Server is running on port http://localhost:' + PORT)
+  console.log(`Server is running on port http://localhost:${PORT} at ${new Date}`)
 })
+ 
