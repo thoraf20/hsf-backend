cd3bddfc5f2bf943b8649155d250d86750c4d146
samson onifade <gbemilekeadeboyega@gmail.com>
2025-06-07 02:47:08 +0100

    seprate concern for elastic search

--- src/config/elasticSearch.config.ts
+++ src/config/elasticSearch.config.ts
@@ -2,7 +2,7 @@ import { Client } from "@elastic/elasticsearch";
 import { getEnv } from "@infrastructure/config/env/env.config";
 
 export const esClient = new Client({
-  node: getEnv('ELASTICSEARCH_NODE') as string,
+  node: getEnv('ELASTICSEARCH_NODE'),
   auth: {
     apiKey: getEnv('ELASTICSEARCH_API_KEY')
   }
