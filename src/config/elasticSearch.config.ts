1a35e6e3bbd4457e51110b920aac812b7d97b7aa
Ayomide Lawal <ayomidelawal700@gmail.com>
2025-06-10 07:36:32 +0100

    fix some minor issue

--- src/config/elasticSearch.config.ts
+++ src/config/elasticSearch.config.ts
@@ -1,10 +1,9 @@
-import { Client } from "@elastic/elasticsearch";
-import { getEnv } from "@infrastructure/config/env/env.config";
+// import { Client } from '@elastic/elasticsearch'
+// import { getEnv } from '@infrastructure/config/env/env.config'
 
-export const esClient = new Client({
-  node: getEnv('ELASTICSEARCH_NODE'),
-  auth: {
-    apiKey: getEnv('ELASTICSEARCH_API_KEY')
-  }
-});
- 
+// export const esClient = new Client({
+//   node: getEnv('ELASTICSEARCH_NODE'),
+//   auth: {
+//     apiKey: getEnv('ELASTICSEARCH_API_KEY'),
+//   },
+// })
--- src/infrastructure/repositories/Elastic Search/ElasticSearchRespository.ts
+++ src/infrastructure/repositories/Elastic Search/ElasticSearchRespository.ts
@@ -1,21 +1,23 @@
-import { elasticSearchRespository } from "@interfaces/ElasticSearchRespository";
-import { esClient } from "@config/elasticSearch.config";
-import { ElasticEnum } from "@domain/enums/propertyEnum";
+import { elasticSearchRespository } from '@interfaces/ElasticSearchRespository'
+// import { esClient } from '@config/elasticSearch.config'
+import { ElasticEnum } from '@domain/enums/propertyEnum'
 export class ElasticSearchRespository implements elasticSearchRespository {
-     async  indexPropertyToES(index: ElasticEnum, object: Record<string, any>): Promise<any>{
-     const store =  await esClient.index({
-             index,
-             document: {
-               ...object,
-               listed_at: new Date().toISOString()
-             }
-           });
-     try {
-         console.log(`Properties ${store._id} is added to elastic search`)
-         return store 
-     } catch (error) {
-         console.log(`Not successfully added : ${error}`)
-     }
-          
-}
+  async indexPropertyToES(
+    index: ElasticEnum,
+    object: Record<string, any>,
+  ): Promise<any> {
+    // const store = await esClient.index({
+    //   index,
+    //   document: {
+    //     ...object,
+    //     listed_at: new Date().toISOString(),
+    //   },
+    // })
+    // try {
+    //   console.log(`Properties ${store._id} is added to elastic search`)
+    //   return store
+    // } catch (error) {
+    //   console.log(`Not successfully added : ${error}`)
+    // }
+  }
 }
