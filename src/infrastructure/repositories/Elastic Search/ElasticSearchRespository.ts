459da936b67b67b5205c5fb7f9cab28b69457fd8
Ayomide Lawal <ayomidelawal700@gmail.com>
2025-06-19 13:51:26 +0100

    disable elastic search

--- src/infrastructure/repositories/Elastic Search/ElasticSearchRespository.ts
+++ src/infrastructure/repositories/Elastic Search/ElasticSearchRespository.ts
@@ -1,44 +1,43 @@
 import { elasticSearchRespository } from '@interfaces/ElasticSearchRespository'
-import { esClient } from '@config/elasticSearch.config'
+// import { esClient } from '@config/elasticSearch.config'
 import { ElasticEnum } from '@domain/enums/propertyEnum'
 export class ElasticSearchRespository implements elasticSearchRespository {
   async indexPropertyToES(
     index: ElasticEnum,
     object: Record<string, any>,
   ): Promise<any> {
-    const store = await esClient.index({
-      index,
-      document: {
-        ...object,
-        listed_at: new Date().toISOString(),
-      },
-    })
+    // const store = await esClient.index({
+    //   index,
+    //   document: {
+    //     ...object,
+    //     listed_at: new Date().toISOString(),
+    //   },
+    // })
     try {
-      console.log(`Properties ${store._id} is added to elastic search`)
-      return store
+      // console.log(`Properties ${store._id} is added to elastic search`)
+      // return store
     } catch (error) {
       console.log(`Not successfully added : ${error}`)
     }
   }
 
   async searchProperties(index: ElasticEnum, query: string): Promise<any> {
-    const property = await esClient.search({
-      index,
-      query: {
-        multi_match: {
-          query,
-          fields: [
-            'property_name^3',
-            'property_description',
-            'street_address',
-            'city',
-            'state',
-          ],
-          type: 'best_fields',
-        },
-      },
-    })
-
-    return property.hits.hits.map((hit) => hit._source)
+    // const property = await esClient.search({
+    //   index,
+    //   query: {
+    //     multi_match: {
+    //       query,
+    //       fields: [
+    //         'property_name^3',
+    //         'property_description',
+    //         'street_address',
+    //         'city',
+    //         'state',
+    //       ],
+    //       type: 'best_fields',
+    //     },
+    //   },
+    // })
+    // return property.hits.hits.map((hit) => hit._source)
   }
 }
