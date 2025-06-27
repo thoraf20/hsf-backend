bbd733098d44d8989d056048e59498a74779bf04
Ayomide Lawal <ayomidelawal700@gmail.com>
2025-06-27 10:08:09 +0100

    fix some issue

--- src/config/email.config.ts
+++ src/config/email.config.ts
@@ -1,41 +1,35 @@
-
-
-import { Email } from "@entities/Email";
+import { Email } from '@entities/Email'
 import {
   TransactionalEmailsApi,
   SendSmtpEmail,
   TransactionalEmailsApiApiKeys,
-} from "@getbrevo/brevo";
-import { getEnv } from "@infrastructure/config/env/env.config";
+} from '@getbrevo/brevo'
+import { getEnv } from '@infrastructure/config/env/env.config'
 
+const BREVO_API_KEY = getEnv('BREVO_API_KEY')
+const DEFAULT_SENDER_NAME = getEnv('COMPANY_NAME') || 'Your Company'
+const DEFAULT_SENDER_EMAIL = getEnv('HSF_SUPPORT_EMAIL')
 
-const BREVO_API_KEY = getEnv('BREVO_API_KEY');
-const DEFAULT_SENDER_NAME = getEnv('COMPANY_NAME') || "Your Company";
-const DEFAULT_SENDER_EMAIL = getEnv('HSF_SUPPORT_EMAIL');
-
-const emailApiInstance = new TransactionalEmailsApi();
-emailApiInstance.setApiKey(
-  TransactionalEmailsApiApiKeys.apiKey,
-  BREVO_API_KEY 
-);
+console.log({ BREVO_API_KEY, DEFAULT_SENDER_EMAIL, DEFAULT_SENDER_NAME })
+const emailApiInstance = new TransactionalEmailsApi()
+emailApiInstance.setApiKey(TransactionalEmailsApiApiKeys.apiKey, BREVO_API_KEY)
 
 export const sendEmail = async (payload: Email): Promise<unknown> => {
-  const sendSmtpEmail = new SendSmtpEmail(); 
+  const sendSmtpEmail = new SendSmtpEmail()
 
-  sendSmtpEmail.subject = payload.subject;
-  sendSmtpEmail.htmlContent = payload.content;
+  sendSmtpEmail.subject = payload.subject
+  sendSmtpEmail.htmlContent = payload.content
 
-  sendSmtpEmail.to = payload.to;
+  sendSmtpEmail.to = payload.to
   sendSmtpEmail.sender = payload.sender ?? {
     name: DEFAULT_SENDER_NAME,
     email: DEFAULT_SENDER_EMAIL,
-  };
+  }
 
-  sendSmtpEmail.replyTo = payload.replyTo;
-  sendSmtpEmail.cc = payload.cc;
-  sendSmtpEmail.bcc = payload.bcc;
-  sendSmtpEmail.tags = payload.tags;
+  sendSmtpEmail.replyTo = payload.replyTo
+  sendSmtpEmail.cc = payload.cc
+  sendSmtpEmail.bcc = payload.bcc
+  sendSmtpEmail.tags = payload.tags
 
-  return emailApiInstance.sendTransacEmail(sendSmtpEmail);
-};
-
+  return emailApiInstance.sendTransacEmail(sendSmtpEmail)
+}
--- src/infrastructure/worker/email.worker.ts
+++ src/infrastructure/worker/email.worker.ts
@@ -1,26 +1,26 @@
-import { Worker } from 'bullmq';
-import { sendEmail } from '@config/email.config';
-import { Email } from '@entities/Email';
-import redis from '@infrastructure/cache/redisClient';
+import { Worker } from 'bullmq'
+import { sendEmail } from '@config/email.config'
+import { Email } from '@entities/Email'
+import redis from '@infrastructure/cache/redisClient'
+import logger from '@middleware/logger'
 
 const emailEmailWorker = new Worker(
   'email-queue',
   async (job) => {
-  try {
-    const emailData = job.data as Email;
-    await sendEmail(emailData);
-    console.log(`Email sent to ${emailData.to[0].email}`);
-  } catch (err) {
-    console.error(`Failed to send email to ${job.data.to}:`, err);
-    throw err; // Let BullMQ retry or log the failure
-  }
-}
-  ,
+    try {
+      const emailData = job.data as Email
+      await sendEmail(emailData)
+      logger.info(`Email sent to ${emailData.to[0].email}`)
+    } catch (err) {
+      logger.error(`Failed to send email to ${job.data.to}:`, err)
+      throw err // Let BullMQ retry or log the failure
+    }
+  },
   {
     connection: redis,
     concurrency: 5, // Adjust based on your needs
     autorun: true, // Automatically start the worker
-  }
-);
+  },
+)
 
 export default emailEmailWorker
